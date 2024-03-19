e88affeb0ccee760c3da9200b6cb6ca0166c2c16
Candy Okoth <44234968+okcandy@users.noreply.github.com>
2024-03-19 23:54:22 +0300

    Updated

--- OlympicGames/SQLQuery1.sql
+++ OlympicGames/SQLQuery1.sql
@@ -19,9 +19,24 @@ SELECT *
 FROM tbl3
 JOIN tbl1 ON tbl1.Total_Summer_Games = tbl3.No_of_Games;
 
+/*rank of gold medal*/
+WITH tbl4 AS 
+	(SELECT Name, COUNT(1) AS Total_Gold_Medals
+	FROM .athlete_events$
+	WHERE Medal = 'Gold'
+	GROUP BY Name),
+tbl5 AS
+	(SELECT *, DENSE_RANK() OVER(ORDER BY Total_Gold_Medals DESC) AS Rank
+	FROM tbl4)
+SELECT *
+FROM tbl5
+WHERE Rank <= 5
+ORDER BY Rank;
 
-
-
-
-
+/*total medal per country*/
+SELECT Regions.region AS Country, Medal, COUNT(1) AS Total_Medals
+FROM .athlete_events$ History
+JOIN .noc_regions$ Regions ON Regions.NOC = History.NOC
+WHERE Medal <> 'NA'
+GROUP BY Regions.region, Medal;
 
